961c31b3607ccae195caa73a89beb4410008afbf
Polina <60664588+PolinaV2020@users.noreply.github.com>
2020-10-20 16:09:48 +0300

    init: only brackets part of brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var i, ch;
+
+    var bracketsMap = new Map();
+    bracketsMap.set("]", "[");
+    bracketsMap.set("}", "{");
+    bracketsMap.set(")", "(");
+
+    var closingBrackets = [...bracketsMap.keys()];
+    var openingBrackets = [...bracketsMap.values()];
+
+    var temp = [];
+    var len = str.length;
+
+    for (i = 0; i < len; i++) {
+        ch = str[i];
+
+        if (openingBrackets.indexOf(ch) > -1) {
+            temp.push(ch);
+        } else if (closingBrackets.indexOf(ch) > -1) {
+            var expectedBracket = bracketsMap.get(ch);
+            if (temp.length === 0 || temp.pop() !== expectedBracket) {
+                return false;
+            }
+        } else {
+            continue;
+        }
+    }
+    return temp.length === 0;
+};
+
 }
